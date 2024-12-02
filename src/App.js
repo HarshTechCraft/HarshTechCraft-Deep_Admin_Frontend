f866248c12529daffeee10ecb18cef95f5312495
HarshWebCraft <harshwebdev26@gmail.com>
2024-12-02 21:28:27 +0530

    route change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Admin from './component/Admin';
+import logo from "./logo.svg";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Admin from "./component/Admin";
 
 function App() {
   return (
     <div className="App">
-      
       <Routes>
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<Admin />} />
       </Routes>
-
     </div>
   );
 }
